016dd60a013baa4b7d2eba38430897d07f33bca3
sangmanla <sangmanla@naver.com>
2018-09-04 15:24:04 -0400

    d

---
+++ seleniumTest/src/main/java/seleniumTest/NaverLoginAndEmailTest.java
@@ -0,0 +1,77 @@
+package seleniumTest;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class NaverLoginAndEmailTest {
+    public static void main(String[] args) {
+	NaverLoginAndEmailTest mine = new NaverLoginAndEmailTest();
+	mine.login();
+	
+	String btnClass = ".btn_upload";
+	mine.waitForThePage(By.cssSelector(btnClass));
+	
+	mine.submitSaveLoginInfo(btnClass);
+	mine.waitForThePage(By.className("ca_item"));
+	
+	mine.goToPageAndWriteEmail();
+	
+    }
+    
+    protected WebDriver driver;
+    protected WebDriverWait wait;
+    
+    public NaverLoginAndEmailTest(){
+	System.setProperty("webdriver.chrome.driver","C:/Program Files (x86)/Google/Chrome/Application/chromedriver.exe");
+	driver = new ChromeDriver();
+	wait = new WebDriverWait(driver, 10);
+    }
+
+    private void login() {
+        driver.get("https://nid.naver.com/nidlogin.login?mode=form&url=https%3A%2F%2Fwww.naver.com");
+        WebElement id = driver.findElement(By.id("id"));
+        WebElement pass = driver.findElement(By.id("pw"));
+        WebElement button = driver.findElement(By.className("btn_global"));         
+
+        id.sendKeys("myId");
+        pass.sendKeys("myPassword");
+        button.submit();
+    }
+    
+    private void goToPageAndWriteEmail() {
+	driver.get("https://mail.naver.com");
+	driver.findElement(By.xpath("//*[@id='nav_snb']/div[1]/a[2]/strong")).click();
+	
+	waitForThePage(By.xpath("//*[@id='subject']"));
+	
+	try{
+	    WebElement subject = driver.findElement(By.xpath("//*[@id='subject']"));
+	    subject.sendKeys("test email");
+	    
+	    driver.switchTo().frame("se2_iframe");
+	    
+	    driver.findElement(By.className("se2_inputarea")).sendKeys("my content");
+	    
+	    driver.switchTo().parentFrame();
+	    driver.findElement(By.id("sendBtn")).click();
+	}catch(Exception e){
+	    e.printStackTrace();
+	}
+    }
+    
+    private void submitSaveLoginInfo(String btnClass) {
+	WebElement button = driver.findElement(By.xpath("//*[@id='frmNIDLogin']/fieldset/span[1]/a"));
+	button.click();
+	WebElement button2 = driver.findElement(By.className("btn_maintain"));
+	button2.click();
+    }
+    
+    private void waitForThePage(By by){
+	WebElement el = this.wait.until(ExpectedConditions.presenceOfElementLocated(by));
+	this.wait.until(ExpectedConditions.visibilityOf(el));
+    }
+}
